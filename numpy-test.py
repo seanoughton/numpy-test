6ff0f86dfc2f525d134d5ed9cf7e8c7d3d59af27
seanoughton <seanoughton@gmail.com>
2019-05-03 13:14:29 -0700

    Cast Python Lists to a Matrix Array (2D Array)

--- numpy-test.py
+++ numpy-test.py
@@ -2,4 +2,9 @@ import numpy as np
 my_list = [1,2,3,4,5,6,7,8,9,10]
 
 my_array = np.array(my_list)
-print(my_array)
+
+
+my_matrix = [[1,2,3],[4,5,6],[7,8,9]]
+my_2d_array = np.array(my_matrix)
+
+print(my_2d_array)
