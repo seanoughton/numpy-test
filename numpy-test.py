f52652140926abf388b95b8f0659b92c6475355f
seanoughton <seanoughton@gmail.com>
2019-05-03 13:16:30 -0700

    using np.arange() to create arrays

--- numpy-test.py
+++ numpy-test.py
@@ -4,7 +4,10 @@ my_list = [1,2,3,4,5,6,7,8,9,10]
 my_array = np.array(my_list)
 
 
-my_matrix = [[1,2,3],[4,5,6],[7,8,9]]
-my_2d_array = np.array(my_matrix)
+my_matrix_1= [[1,2,3],[4,5,6],[7,8,9]]
+my_2d_array = np.array(my_matrix_1)
 
-print(my_2d_array)
+my_matrix_2 = np.arange(0,10)
+my_matrix_3 = np.arange(0,10,2)
+
+print(my_matrix_3)
